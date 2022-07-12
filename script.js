5d42de0865a49801d4942d2deb64302e66310fef
Haule9-2 <76088882+Haule9-2@users.noreply.github.com>
2022-07-12 16:14:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var names = ["Hau","Johm","Mary","jimn","Henry"] 
+
+for (var i=0; i< names.length; i++)
+{    if ((names[i].charAt(0)).toLowerCase() == "j" )
+        console.log("Goodbye " + names[i])
+    else
+        console.log("Hello " + names[i])
+}
+
